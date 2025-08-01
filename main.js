a54853f513d8ab9ecc1b4bea31473fa597add2fa
Jeremy Wiles-Young <jwilesyoung@gmail.com>
2025-08-01 15:44:53 -0400

    make buttons and stories work

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 window.Webflow ||= [];
 
 window.Webflow.push(() => {
+  let seasonState = "season";
+
   // place buttons on the map according to the CMS coordinates
 	document.querySelectorAll(".kwm-button").forEach(el => {
     const x = parseFloat(el.dataset.x);
@@ -14,23 +16,83 @@ window.Webflow.push(() => {
     el.addEventListener("click", () => {
       const type = el.dataset.type;
       handleSeason(type);
-    })
+    });
   });
 
+  // give the button overlays story action
+  document.querySelectorAll(".kwm-button").forEach(el => {
+    el.addEventListener("click", () => {
+      const name = el.dataset.name;
+      handleDetail(name);
+    });
+  }); 
+  
+  // handle click on button to show story
+  function handleDetail(name) {
+    // console.log("name", name); 
+    // show the name story
+    document.querySelectorAll(".kwm-story").forEach(el => {
+      console.log("log the items: ", el);
+      if(el.dataset.name === name) {
+        el.style.display = "grid";
+        el.style.opacity = "1";
+        el.style.pointerEvents = "auto";
+        // select and change the blurbs in the story
+        document.querySelectorAll(".kwm-story-blurb-rt").forEach(el => {
+          if(el.dataset.type === seasonState) {
+            el.style.opacity = "1";
+            el.style.position = "relative";
+          } else {
+            el.style.opacity = '0';
+            el.style.position = "absolute";
+          }
+        }); 
+        el.addEventListener("click", () => {
+          el.style.display = "none";
+          el.style.opacity = "0";
+          el.style.pointerEvents = "none";
+        })
+      } else {
+        el.style.display = "none";
+        el.style.opacity = "0";
+        el.style.pointerEvents = "none";
+      }
+    })
+  }
 
-})  
+  
 
-// function to change the bg image and the refs to the texts
-function handleSeason( season ) {
-  // change the bg
-  // select the bg wrapper, change opacity
-  document.querySelectorAll(".kwm-bg").forEach(el => {
-    if(el.dataset.type === season) {
-      el.style.opacity = "1";
-    } else {
-      el.style.opacity = '0';
-    }
-  })
+  // function to change the bg image and the refs to the texts
+  function handleSeason( season ) {
+    seasonState = season;
+    // change the bg
+    // select the bg wrapper, change opacity
+    document.querySelectorAll(".kwm-bg").forEach(el => {
+      if(el.dataset.type === season) {
+        el.style.opacity = "1";
+      } else {
+        el.style.opacity = '0';
+      }
+    });
 
+    // make the button selected
+    document.querySelectorAll(".season-button1").forEach(el => {
+      if(el.dataset.type === season) {
+          el.classList.add('selected');
+        } else {
+          el.classList.remove('selected');
+        }
+    });
 
-}
+    // select and change the blurbs in the story
+    document.querySelectorAll(".kwm-story-blurb-rt").forEach(el => {
+      if(el.dataset.type === season) {
+        el.style.opacity = "1";
+        el.style.position = "relative";
+      } else {
+        el.style.opacity = '0';
+        el.style.position = "absolute";
+      }
+    }); 
+  }
+});
